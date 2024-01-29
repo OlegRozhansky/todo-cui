61141d134ff8dfa2b8d3a3cfd25c66d0d3ff324e
OlegRozhansky <oleg.rozhansky@gmail.com>
2024-01-29 22:07:01 +0200

    create gui

--- gui.py
+++ gui.py
@@ -15,6 +15,6 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
         break
-    print('You entered ', values[0])
+    print('You entered ', values)
 
 window.close()
