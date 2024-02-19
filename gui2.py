a3cd2c59ca5344dd3104459385af38aba5a0efd1
OlegRozhansky <oleg.rozhansky@gmail.com>
2024-02-20 00:32:20 +0200

    re-create gui, add behavior

--- gui2.py
+++ gui2.py
@@ -2,7 +2,7 @@ import PySimpleGUI as sg
 from modules import functions
 
 label = sg.Text('Type in a to-do')
-input_box = sg.InputText()
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 exit_button = sg.Button("Exit")
 
@@ -11,6 +11,15 @@ window = sg.Window('To Do App',
                    layout=[[label, input_box, add_button], [exit_button]],
                    font=('Helvetica', 20))
 # Event Loop to process "events" and get the "values" of the inputs
-window.read()
-print("Hello")
+while True:
+    event, values = window.read()
+    print(event, values)
+    match event:
+        case 'Exit' | None:
+            break
+        case 'Add':
+            todos = functions.get_todos()
+            todos.append(values['todo'] + '\n')
+            functions.write_todos(p_todos=todos)
+
 window.close()
