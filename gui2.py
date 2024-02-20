4bb9aebd1c4a459ac8c10775fed68eefd818e643
OlegRozhansky <oleg.rozhansky@gmail.com>
2024-02-20 02:55:04 +0200

    complete gui

--- gui2.py
+++ gui2.py
@@ -8,10 +8,11 @@ exit_button = sg.Button("Exit")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=(45, 10))
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
 
 # Create the Window
 window = sg.Window('To Do App',
-                   layout=[[label], [input_box, add_button], [list_box, edit_button], [exit_button]],
+                   layout=[[label], [input_box, add_button], [list_box, edit_button, complete_button], [exit_button]],
                    font=('Helvetica', 20))
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
@@ -38,5 +39,12 @@ while True:
                 functions.write_todos(p_todos=todos)
                 #window['todos'].update(todos)
                 list_box.update(todos)
+        case 'Complete':
+            if len(values['todos']) != 0:
+                todos = functions.get_todos()
+                todos.remove(values['todos'][0])
+                functions.write_todos(p_todos=todos)
+                list_box.update(todos)
+                window['todo'].update('')
 
 window.close()
